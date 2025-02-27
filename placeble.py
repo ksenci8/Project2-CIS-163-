b93f9c415de6341bd4031b7fc1b10dabf6051aff
ksenci8 <ksencilo88@gmail.com>
2025-02-27 12:58:12 -0500

    Add files via upload

--- placeble.py
+++ placeble.py
@@ -1,12 +1,32 @@
-#maybe just import the files into this one.
+
+#work by Ksenija (from 26th to 27th of February)
+#wrote class Placeble
 
 from enum import Enum
 from abc import ABC, abstractmethod
 from typing import List, Optional
 
-# from player_colors import PlayerColors
-# from position import Position
-# from go_gui_view import GUI
+class PlayerColors(Enum):
+    BLACK = 0
+    WHITE = 1
+
+    def opponent(self):
+        cls = self.__class__
+        members = list(cls)
+        index = members.index(self) + 1
+        if index >= len(members):
+            index = 0
+        return members[index]
+class Position:
+    def __init__(self, row, col):
+        if not isinstance(row, int) or not isinstance(col, int):
+            raise TypeError
+        self.row = row
+        self.col = col
+
+    def __str__(self):
+        output = f'Placed Go Game piece at [{self.row}, {self.col}]'
+        return output
 
 class Placeble(ABC):
     def __init__(self, color):
@@ -21,6 +41,6 @@ class Placeble(ABC):
     def __str__(self):
         pass
     @abstractmethod
-    def is_valid_placement(self, pos: Position, board: List[List[Optional[GamePiece]]]):
+    def is_valid_placement(self, pos: Position, board):
         pass
 
