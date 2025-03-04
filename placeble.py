1921aaf3ec23cb8d60ac51f7c3fc673265982718
ksenci8 <ksencilo88@gmail.com>
2025-03-04 11:08:56 -0500

    Add files via upload

--- placeble.py
+++ placeble.py
@@ -2,31 +2,24 @@
 #work by Ksenija (from 26th to 27th of February)
 #wrote class Placeble
 
-from enum import Enum
+#work by Ksenija (from 26th to 27th of February)
+#wrote class Placeble
+#Should we do a more concise method now that we both understand what we are doing up here like this:
+#Caleb 02/27
+#fixed functionality of code working with starting code provided for the project
+#Ksenija 03/04
+#Fixed small typo in is_valid_placement (was if pos.col < 0 or pos.row >= len(board[0])
+#changed to if pos.col < 0 or pos.col >= len(board[0]))
+
+
+#starter code imports
+from player_colors import PlayerColors
+from position import Position
+
+#our code imports
 from abc import ABC, abstractmethod
 from typing import List, Optional
-
-class PlayerColors(Enum):
-    BLACK = 0
-    WHITE = 1
-
-    def opponent(self):
-        cls = self.__class__
-        members = list(cls)
-        index = members.index(self) + 1
-        if index >= len(members):
-            index = 0
-        return members[index]
-class Position:
-    def __init__(self, row, col):
-        if not isinstance(row, int) or not isinstance(col, int):
-            raise TypeError
-        self.row = row
-        self.col = col
-
-    def __str__(self):
-        output = f'Placed Go Game piece at [{self.row}, {self.col}]'
-        return output
+from operator import truediv
 
 class Placeble(ABC):
     def __init__(self, color):
@@ -42,5 +35,15 @@ class Placeble(ABC):
         pass
     @abstractmethod
     def is_valid_placement(self, pos: Position, board):
-        pass
+        if not isinstance(pos, Position):
+            return False
 
+        #checking if the position is within the board
+        if pos.row < 0 or pos.row >= len(board):
+            return False
+        if pos.col < 0 or pos.col >= len(board[0]):
+            return False
+        #checking if the place on board is empty
+        if board[pos.row][pos.col] is not None:
+            return False
+        return True
