eac888246ef923389537cd611c91736e6ecb12f0
ksenci8 <ksencilo88@gmail.com>
2025-03-07 13:06:23 -0500

    Step 2 (is_valid_placement)
    - Polished first/last row and column
    - Added booleans after checking
    - Reset the occupied_count back to 0 so it does not overcount

--- game_piece.py
+++ game_piece.py
@@ -17,50 +17,67 @@ class GamePiece(Placeble):
         return f'Color: {self.color}'
 
     def is_valid_placement(self, pos: Position, board):
-        left = board[pos.row][pos.col-1]
-        right = board[pos.row][pos.col+1]
-        up = board[pos.row-1][pos.col]
-        down = board[pos.row+1][pos.col]
+        if not super().is_valid_placement(pos, board):
+            return False
+        left = board[pos.row][pos.col-1].color
+        right = board[pos.row][pos.col+1].color
+        up = board[pos.row-1][pos.col].color
+        down = board[pos.row+1][pos.col].color
         occupied_count = 0
         player_piece = PlayerColors.BLACK
         opponent_piece = player_piece.opponent()
-        #checking left
-        if left == opponent_piece:
-            occupied_count +=1
-        #checking right
-        if right == opponent_piece:
-            occupied_count +=1
-        #checking up
-        if up == opponent_piece:
-            occupied_count +=1
-        #checking down
-        if down == opponent_piece:
-            occupied_count +=1
-        #Checking for corner scenarios, three places need to be occupied
+
+        # Last row and column index
         last_row = len(board) - 1
         last_col = len(board[0]) - 1
-        #First row
-        if pos.row == 0:
-            if pos.row > 0 and up == opponent_piece:
+
+        # Checking for edge case scenarios (corners)
+        if pos.row == 0 or pos.row == last_row: #First or last row
+            if up == opponent_piece:
                 occupied_count +=1
-            if pos.col < last_col and right == opponent_piece:
+            if pos.row < last_row and right == opponent_piece:
                 occupied_count += 1
             if down == opponent_piece:
                 occupied_count += 1
-        #Last row
 
-        #will be checked last
-        if occupied_count == 4:
+            if left == opponent_piece:
+                occupied_count += 1
+
+        #First or last column
+        if pos.col == 0 or pos.col == last_col:
+            if up == opponent_piece:
+                occupied_count += 1
+            if right == opponent_piece:
+                occupied_count +=1
+            if down == opponent_piece:
+                occupied_count += 1
+            if left == opponent_piece:
+                occupied_count += 1
+
+        if occupied_count == 3:
             return True
 
+        #The general scenario, checking for four places
+        occupied_count = 0
+        # checking left
+        if left == opponent_piece:
+            occupied_count += 1
+        # checking right
+        if right == opponent_piece:
+            occupied_count += 1
+        # checking up
+        if up == opponent_piece:
+            occupied_count += 1
+        # checking down
+        if down == opponent_piece:
+            occupied_count += 1
+        # will be checked last
+        if occupied_count == 4:
+            return True
+        return False
 
-
-
-
-
-
-        ''' I believe this will just use the parent function and then add on the checking for when
-        the user is trying to place a piece in a spot that is surrounded by the opponent's pieces'''
+        # ''' I believe this will just use the parent function and then add on the checking for when
+        # the user is trying to place a piece in a spot that is surrounded by the opponent's pieces'''
 
 
 
--- game_player.py
+++ game_player.py
@@ -1,2 +1,3 @@
 class GamePlayer:
     pass
+    
