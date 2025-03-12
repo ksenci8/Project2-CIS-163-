f6ff5b3787620f412af781997df13f4e8d893209
Ic3Vortex <calebb2202@gmail.com>
2025-03-11 21:35:30 -0400

    placeble.py and game_piece.py
    All I did for placeble.py was add docstrings. 
    For game_piece.py I struggled trying to create the is_valid_placement check for if the position is surrounded. I wasn't able to make it so ignore my code there keep yours. But I did make the equals method working. with the given example she has in step 2 to test I found out that she actually means "__eq__" for the method and not literally "equals".
    
    is_valid_placement's test case just results in an error and I narrowed the error to be coming from one specific line of code in my version of is_valid_placement but even with that narrowed down I couldn't find the issue so I am going to email the professor.

--- game_piece.py
+++ game_piece.py
@@ -1,14 +1,14 @@
-#Ksenija from 02/26 to 02/27
+#Ksenija 2/26-2/27
 #wrote class GamePiece
 #Caleb 02/27
 #fixed fuctionality of code working with starting code provided for the project
-#Ksenija 03/01
-#Wrote out the majority of the logic for checking if player is occupied by opponent
-#in is_valid_placement
+#Caleb 03/11
+#created is_valid_placement method
 
 #starter code imports
 from position import Position
 from player_colors import PlayerColors
+
 #our code imports
 from placeble import Placeble
 
@@ -17,78 +17,64 @@ class GamePiece(Placeble):
         return f'Color: {self.color}'
 
     def is_valid_placement(self, pos: Position, board):
-        if not super().is_valid_placement(pos, board):
-            return False
-        left = board[pos.row][pos.col-1].color
-        right = board[pos.row][pos.col+1].color
-        up = board[pos.row-1][pos.col].color
-        down = board[pos.row+1][pos.col].color
-        occupied_count = 0
-        player_piece = PlayerColors.BLACK
-        opponent_piece = player_piece.opponent()
-
-        # Last row and column index
         last_row = len(board) - 1
         last_col = len(board[0]) - 1
 
-        # Checking for edge case scenarios (corners)
-        if pos.row == 0 or pos.row == last_row: #First or last row
-            if up == opponent_piece:
-                occupied_count +=1
-            if pos.row < last_row and right == opponent_piece:
-                occupied_count += 1
-            if down == opponent_piece:
-                occupied_count += 1
+        if not super().is_valid_placement(pos, board):
+            return False
 
-            if left == opponent_piece:
-                occupied_count += 1
+        if pos.col != 0:
+            left = board[pos.row][pos.col - 1]
+        else:
+            return False
+        if pos.col != last_col:
+            right = board[pos.row][pos.col + 1]
+        else:
+            return False
+        if pos.row != 0:
+            up = board[pos.row - 1][pos.col]
+        else:
+            return False
+        if pos.col != last_row:
+            down = board[pos.row + 1][pos.col]
+        else:
+            return False
 
-        #First or last column
-        if pos.col == 0 or pos.col == last_col:
-            if up == opponent_piece:
-                occupied_count += 1
-            if right == opponent_piece:
-                occupied_count +=1
-            if down == opponent_piece:
-                occupied_count += 1
-            if left == opponent_piece:
-                occupied_count += 1
 
-        if occupied_count == 3:
+
+        if left is None:
+            return True
+        if right is None:
+            return True
+        if up is None:
+            return True
+        if down is None:
             return True
 
-        #The general scenario, checking for four places
-        occupied_count = 0
-        # checking left
-        if left == opponent_piece:
-            occupied_count += 1
-        # checking right
-        if right == opponent_piece:
-            occupied_count += 1
-        # checking up
-        if up == opponent_piece:
-            occupied_count += 1
-        # checking down
-        if down == opponent_piece:
-            occupied_count += 1
-        # will be checked last
-        if occupied_count == 4:
-            return True
-        return False
 
-        # ''' I believe this will just use the parent function and then add on the checking for when
-        # the user is trying to place a piece in a spot that is surrounded by the opponent's pieces'''
+        if board[pos.row][pos.col] != left and right and up and down:
+            return False
+        return True
+
+        # if board[pos.row][pos.col] != board[pos.row][pos.col-1] or board[pos.row][pos.col+1] or board[pos.row-1][pos.col] or board[pos.row+1][pos.col]:
+        #     return False
+        # return True
+
 
 
 
     #checking if two instances of GamePiece have the same color/value
-    def equals(self, other):
-       if not isinstance(other, GamePiece):
-           return False
-       return True
+    def __eq__(self, other):
+        if self.color == other.color:
+           return True
+        return False
 
-# gamePiece1 = GamePiece(PlayerColors.BLACK)
-# gamePiece2 = GamePiece(PlayerColors.BLACK)
-# gamePiece3 = GamePiece(PlayerColors.WHITE)
-# print(gamePiece1 == gamePiece2) # prints True
-# print(gamePiece1 == gamePiece3) # prints False
+
+
+
+gamePiece1 = GamePiece(PlayerColors.BLACK)
+gamePiece2 = GamePiece(PlayerColors.BLACK)
+gamePiece3 = GamePiece(PlayerColors.WHITE)
+print(gamePiece1 == gamePiece2) # prints True
+print(gamePiece1 == gamePiece3) # prints False
+
--- placeble.py
+++ placeble.py
@@ -2,15 +2,15 @@
 #work by Ksenija (from 26th to 27th of February)
 #wrote class Placeble
 
-#work by Ksenija (from 26th to 27th of February)
+#Ksenija 02/26-02/27
 #wrote class Placeble
-#Should we do a more concise method now that we both understand what we are doing up here like this:
 #Caleb 02/27
 #fixed functionality of code working with starting code provided for the project
 #Ksenija 03/04
 #Fixed small typo in is_valid_placement (was if pos.col < 0 or pos.row >= len(board[0])
 #changed to if pos.col < 0 or pos.col >= len(board[0]))
-
+#Caleb 03/11
+#added docstrings to file
 
 #starter code imports
 from player_colors import PlayerColors
@@ -22,22 +22,59 @@ from typing import List, Optional
 from operator import truediv
 
 class Placeble(ABC):
+    '''
+    creates color property and the abstractmethod for is_valid_placement
+
+    Attributes:
+        __color (PlayerColors): Object that is the color of the piece
+    Methods:
+        color(): property get method that returns __color
+        __str__(): passes (no current str for class)
+        is_valid_placement(pos, board): checks if pos is withing the confines of the board
+        then checks if the position is empty if both are met returns True
+    '''
     def __init__(self, color):
+        '''
+        Initializes the Placeable object and checks instance of color
+
+        Args:
+            __color(PlayerColors): Object that is the color of the piece
+        Raises:
+            TyperError: if color is not a PlayerColors object
+        '''
         if not isinstance(color, PlayerColors):
             raise TypeError
         self.__color = color
 
     @property
     def color(self):
+        '''
+        Returns the __color value
+
+        Returns:
+             PlayerColors: The __color object
+        '''
         return self.__color
+
     @abstractmethod
     def __str__(self):
+        '''
+        Abstract method that will be used by subclasses
+        '''
         pass
+
     @abstractmethod
     def is_valid_placement(self, pos: Position, board):
+        '''
+        The Parent Method for checking validation of placement. Checks if pos is a position on
+        the board then checks if the position is empty
+
+        Returns:
+            Bool: if conditions not met False if conditions met True
+        '''
+
         if not isinstance(pos, Position):
             return False
-
         #checking if the position is within the board
         if pos.row < 0 or pos.row >= len(board):
             return False
