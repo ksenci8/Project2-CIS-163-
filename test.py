edc65b5dfcc43a650926acfb082da6b3f8886a12
Ic3Vortex <calebb2202@gmail.com>
2025-03-23 22:20:17 -0400

    test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ from position import Position
 from go_model import GoModel, UndoException
 from game_piece import GamePiece
 from game_player import GamePlayer
+import copy
 
 class TestGoStep2(unittest.TestCase):
     def test_check_if_valid(self):
@@ -76,11 +77,11 @@ class TestGoStep3(unittest.TestCase):
     def constructor(self):
         p = GamePlayer()
         with self.assertRaises(TypeError):
-            p.capture_count('hello')
+            p.capture_count('send')
         with self.assertRaises(TypeError):
             p.skip_count(kjk)
         with self.assertRaises(TypeError):
-            p.player_color('black')
+            p.player_color('help')
 
 
 class TestGoStep4(unittest.TestCase):
@@ -143,7 +144,7 @@ class TestGoStep4(unittest.TestCase):
         self.assertEqual(self.game.consecutive_pass, 2)
     def test_is_valid_placement(self):
         with self.assertRaises(TypeError):
-            self.game.is_valid_placement('jdjadjadh')
+            self.game.is_valid_placement('can you hear me')
     def test_is_game_over(self):
         self.game.pass_turn()
         self.game.pass_turn()
@@ -167,16 +168,17 @@ class TestGoStep4(unittest.TestCase):
     def test_undo_raises_exception(self):
         with self.assertRaises(UndoException):
             self.game.undo()
-    # def test_set_piece(self):
-    #     game = GoModel(6,6)
-    #     piece = GamePiece(PlayerColors.WHITE)
-    #     pos = Position(1,1)
-    #     # Set the piece at the given position
-    #     game.set_piece(pos, piece)
-    #
-    #     # Check if the piece is placed at the correct position
-    #     self.assertEqual(game.board[pos.row][pos.col], piece)
-    #
+
+    def test_set_piece(self):
+        game = GoModel(6,6)
+        piece = GamePiece(PlayerColors.WHITE)
+        pos = Position(1,1)
+        # Set the piece at the given position
+        game.set_piece(pos, piece)
+
+        # Check if the piece is placed at the correct position
+        self.assertEqual(game.board[pos.row][pos.col], piece)
+
     def test_capture(self):
         black_piece = GamePiece(PlayerColors.BLACK)
         white_piece = GamePiece(PlayerColors.WHITE)
@@ -232,7 +234,7 @@ class TestGoStep4(unittest.TestCase):
         self.game.set_piece(Position(1, 2), GamePiece(PlayerColors.BLACK))
         initial_count = self.game.player1.capture_count
         self.game.capture()
-        self.assertEqual(self.game.player1.capture_count, initial_count + 1) # 1 black should capture, might be too early to work
+        self.assertEqual(self.game.current_player.capture_count, initial_count + 1)
 
     def test_capture_count_group(self):
         #White group surrounded by black group
@@ -247,15 +249,16 @@ class TestGoStep4(unittest.TestCase):
         self.game.set_piece(Position(2, 1), GamePiece(PlayerColors.BLACK))
         self.game.set_piece(Position(2, 2), GamePiece(PlayerColors.BLACK))
 
-        initial_count = self.game.player1.capture_count
+
+        initial_count = self.game.current_player
         self.game.capture()
-        self.assertEqual(self.game.player1.capture_count, initial_count + 2)#should be 2, might not work
+        self.assertEqual(self.game.current_player.capture_count, 0 + 2)#should be 2, might not work
 
     def test_capture_count_no_capture(self):
         #White piece with liberties
 
         self.game.player1 = GamePlayer(PlayerColors.BLACK)
-        self.game.player2 = GamePlayer(PlayerColors.WHITE)
+        self.game.player1 = GamePlayer(PlayerColors.WHITE)
         self.game.set_piece(Position(1, 1), GamePiece(PlayerColors.WHITE))
         self.game.set_piece(Position(0, 1), GamePiece(PlayerColors.BLACK))
         self.game.set_piece(Position(1, 0), GamePiece(PlayerColors.BLACK))
